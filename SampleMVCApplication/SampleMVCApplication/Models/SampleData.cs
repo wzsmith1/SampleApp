d87f62988a2dfc50488c788974ede57d23c3da4d
wzsmith1 <wzsmith1@yahoo.com>
2014-06-25 17:31:29 -0400

    Added files

---
+++ SampleMVCApplication/SampleMVCApplication/Models/SampleData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleMVCApplication.Models
+{
+    public class SampleData
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string location { get; set; }
+    }
+}
---
+++ SampleMVCApplication/SampleMVCApplication/Scripts/Home/home.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('.btn-primary').click(function () {
+        $('.alert-danger').hide();
+        $('.alert-success').show();
+    });
+    $('.btn-default').click(function () {
+        $('.alert-success').hide();
+        $('.alert-danger').show();
+    });
+
+});
